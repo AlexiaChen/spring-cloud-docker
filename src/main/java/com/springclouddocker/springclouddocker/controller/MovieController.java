2bc2cadc7129055eb17692eab3e9eaf8f0fbe3d9
MathxH Chen <brainfvck@foxmail.com>
2019-03-02 10:25:48 +0800

    [#] UserService: Avoid hard code of User Service URL

--- src/main/java/com/springclouddocker/springclouddocker/controller/MovieController.java
+++ src/main/java/com/springclouddocker/springclouddocker/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.springclouddocker.springclouddocker.controller;
 
 import com.springclouddocker.springclouddocker.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,9 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MovieController {
 
+    @Value("${user.userServiceUrl}")
+    private String userServiceUrl;
+
     @Autowired
     private RestTemplate restTemplate;
 
@@ -23,6 +27,6 @@ public class MovieController {
     public User findById(@PathVariable Long id){
 
         // movie service call user service findById API. User service in another process in distributed env
-        return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+        return this.restTemplate.getForObject(this.userServiceUrl + id, User.class);
     }
 }
