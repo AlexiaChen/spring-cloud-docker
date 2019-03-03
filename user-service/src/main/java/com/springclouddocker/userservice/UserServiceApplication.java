29600b1891a7a3c90b10f02982ff6d2e78831fb1
MathxH Chen <brainfvck@foxmail.com>
2019-03-03 13:15:04 +0800

    [#] UserService: Register the service to eureka server

--- user-service/src/main/java/com/springclouddocker/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/springclouddocker/userservice/UserServiceApplication.java
@@ -2,12 +2,13 @@ package com.springclouddocker.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Or you can use @EnableEurekaClient.  but @EnableDiscoveryClient is portable between Eureka, Zookeeper, Consul
 public class UserServiceApplication {
-
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
