b6a7cb099fdc9c5ab9d0b583acae091d4770c327
MathxH <brainfvck@foxmail.com>
2019-03-10 10:51:37 +0800

    [+] UserService: Add my metadata, MovieService fetch it from UserService0

--- movie-service/src/main/java/com/springclouddocker/movieservice/controller/MovieController.java
+++ movie-service/src/main/java/com/springclouddocker/movieservice/controller/MovieController.java
@@ -3,10 +3,14 @@ package com.springclouddocker.movieservice.controller;
 import com.springclouddocker.movieservice.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 
 /**
@@ -23,10 +27,19 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id){
 
         // movie service call user service findById API. User service in another process in distributed env
         return this.restTemplate.getForObject(this.userServiceUrl + id, User.class);
+    }
+
+    @GetMapping("/user-service-instance/info")
+    public List<ServiceInstance> showInfo(){
+        // you will find your metadata define in user-service from returned json
+        return this.discoveryClient.getInstances("microservice-user-service");
     }
 }
--- user-service/src/main/java/com/springclouddocker/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/springclouddocker/userservice/UserServiceApplication.java
@@ -3,9 +3,10 @@ package com.springclouddocker.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient // Or you can use @EnableEurekaClient.  but @EnableDiscoveryClient is portable between Eureka, Zookeeper, Consul
+@SpringBootApplication
 public class UserServiceApplication {
 	public static void main(String[] args) {
 
