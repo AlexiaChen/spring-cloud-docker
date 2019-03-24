b5a69089a4042bace8ffa30977c4f2ca0e099acd
MathxH <brainfvck@foxmail.com>
2019-03-24 11:04:17 +0800

    [+] MovieService: Create Feign manually for select user role of REST API of User Service

--- movie-service/src/main/java/com/springclouddocker/movieservice/controller/MovieController.java
+++ movie-service/src/main/java/com/springclouddocker/movieservice/controller/MovieController.java
@@ -2,6 +2,12 @@ package com.springclouddocker.movieservice.controller;
 
 import com.springclouddocker.movieservice.domain.User;
 import com.springclouddocker.movieservice.feign.UserFeignClient;
+import feign.Client;
+import feign.Contract;
+import feign.Feign;
+import feign.auth.BasicAuthRequestInterceptor;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +15,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +29,7 @@ import java.util.List;
 *  Actually, In product enviroment. Movie Service is one process on a Server (separated from User Service with a process on a server)
 */
 
+@Import(FeignClientsConfiguration.class)
 @RestController
 public class MovieController {
 
@@ -29,8 +38,8 @@ public class MovieController {
     @Value("${user.userServiceUrl}")
     private String userServiceUrl;
 
-    @Autowired
-    private UserFeignClient userFeignClient;
+    private UserFeignClient userUserFeignClient;
+    private UserFeignClient adminUserFeignClient;
 
     @Autowired
     private DiscoveryClient discoveryClient;
@@ -38,11 +47,27 @@ public class MovieController {
     @Autowired
     private LoadBalancerClient loadBalancerClient;
 
-    @GetMapping("/user/{id}")
-    public User findById(@PathVariable Long id){
+    @Autowired
+    public MovieController(Decoder decoder, Encoder encoder, Client client, Contract contract){
+        this.userUserFeignClient = Feign.builder().client(client).encoder(encoder).decoder(decoder).contract(contract)
+                .requestInterceptor(new BasicAuthRequestInterceptor("user", "user123")).target(UserFeignClient.class, userServiceUrl);
+        this.adminUserFeignClient = Feign.builder().client(client).encoder(encoder).decoder(decoder).contract(contract)
+                .requestInterceptor(new BasicAuthRequestInterceptor("admin", "admin123"))
+                .target(UserFeignClient.class, userServiceUrl);
+    }
+
+    @GetMapping("/user-user/{id}")
+    public User findByUserId(@PathVariable Long id){
 
         // movie service call user service findById API. User service in another process in distributed env
-        return this.userFeignClient.findById(id);
+        return this.userUserFeignClient.findById(id);
+    }
+
+    @GetMapping("/user-admin/{id}")
+    public User findByAdminId(@PathVariable Long id){
+
+        // movie service call user service findById API. User service in another process in distributed env
+        return this.adminUserFeignClient.findById(id);
     }
 
     @GetMapping("/user-service-instance/info")
