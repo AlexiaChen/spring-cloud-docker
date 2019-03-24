b4719d283dafce7f397961658e9ae9b10b32ded8
MathxH <brainfvck@foxmail.com>
2019-03-24 10:36:24 +0800

    [+] UserService: Add logger

--- user-service/src/main/java/com/springclouddocker/userservice/controller/UserController.java
+++ user-service/src/main/java/com/springclouddocker/userservice/controller/UserController.java
@@ -2,10 +2,17 @@ package com.springclouddocker.userservice.controller;
 
 import com.springclouddocker.userservice.domain.User;
 import com.springclouddocker.userservice.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 /**
  *  User Micro-Service
@@ -17,8 +24,25 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+
+    private void printUserAuth(UserDetails userDetails){
+        Collection<? extends GrantedAuthority> collection = userDetails.getAuthorities();
+        for(GrantedAuthority c : collection){
+            UserController.LOGGER.info("Current user is {}, Current role is {}", userDetails.getUsername(),
+                    c.getAuthority());
+        }
+    }
+
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id) {
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal instanceof UserDetails){
+            printUserAuth((UserDetails) principal);
+        }
+
         User user = this.userRepository.findOne(id);
         return user;
     }
